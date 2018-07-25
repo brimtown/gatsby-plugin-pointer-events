823a7c3b301379fcf4e81096499ab798cc96a0e0
Tim Brown <brown.tim.lee@gmail.com>
2018-07-25 11:50:51 -0400

    Remove ES2015 to prevent Uglify errors
    * The versions of Uglify and Webpack that Gatsby 1 uses
      cannot parse ES2015
    * This would throw an error when Uglify was run in a production context
      with `gatsby build`
    * Switch to `function` syntax to avoid throwing an error during a
      production Gatsby build

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-exports.onClientEntry = () => {
+exports.onClientEntry = function() {
   if (!window.PointerEvent) {
     require.ensure([], function(require) { require('pepjs').default }, 'pepjs')
   }
